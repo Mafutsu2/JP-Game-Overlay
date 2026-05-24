78b09471e3a17145270ccff6a04279e26abf34cf
mafutsu2 <mafutsu2@gmail.com>
2026-05-23 20:08:58 -0400

    add possibility to change and save options to user data folder

---
+++ constants.js
@@ -0,0 +1,12 @@
+export const OPTIONS_FILE = 'userOptions.json';
+
+export const ALWAYS = 0;
+export const TOGGLE = 1;
+export const HOLD = 2;
+
+export const DEFAULT_WINDOW = { id: -1, title: '--None--', owner: '--None--' };
+export const DEFAULT_DELAY = 2;
+export const DEFAULT_YOMITAN_HOTKEY = 'Alt+Shift+Y';
+export const DEFAULT_EXIT_HOTKEY = 'Alt+Shift+Q';
+export const DEFAULT_ACTIVATION_HOTKEY = 'Tab';
+export const DEFAULT_ACTIVATION = ALWAYS;
